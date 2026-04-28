3811aab404d6a785f4999b2b69f02f6b93a8494e
Piyush Pal <piyushpal2685@gmail.com>
2026-04-28 12:51:00 +0530

    2commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return send_from_directory('.', 'index.html')
+
+@app.route("/dashboard")
+def dashboard():
+    return send_from_directory('.', 'dashboard.html')
